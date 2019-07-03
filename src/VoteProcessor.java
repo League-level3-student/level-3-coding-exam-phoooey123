e64ec4b467174a01479abe8b04b1fb209c338e29
league <league@imac-11.attlocal.net>
2019-07-02 20:04:15 -0700

    redid most of vote processor

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,28 +1,21 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class VoteProcessor {
 String calculateElectionWinner(ArrayList<String> votes) {
-	int cand1 = 0;
-	int cand2 = 0;
-	for (int i = 0; i < votes.size(); i++) {
-		
-		if(votes.get(i).equals("Pope francis") || votes.get(i).equals("pope francis") || votes.get(i).equals("Pope Francis") || votes.get(i).equals("pope Francis") || votes.get(i).equals("POPE FRANCIS")) {
-			cand1++;
-		}
-		if(votes.get(i).equals("Edward snowden") || votes.get(i).equals("edward snowden") || votes.get(i).equals("Edward Snowden") || votes.get(i).equals("edward Snowden") || votes.get(i).equals("EDWARD SNOWDEN")) {
-			cand2++;
-		}
-		
-	}
-	if(cand1>cand2) {
-		return "pope francis";
-	}
-	if(cand2>cand1) {
-		return "edward snowden";
-	}
-	else {
-		return "TIE";
-	}
 	
+	for (int i = 0; i < votes.size(); i++) {
+		votes.set(i, votes.get(i).toLowerCase());
+		HashMap<String, Integer>  hm = new HashMap<String, Integer>();
+		String can = votes.get(i);
+		if(hm.containsKey(can)) {
+			hm.put(can, hm.get(can)+1);
+		}
+		else {
+			hm.put(can, 1);
+		}
+		
+	}
+	return "";
 }
 }
